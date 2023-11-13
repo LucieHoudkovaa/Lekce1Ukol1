a0178b8edd77341b4e96c5eec5fd1d1a3d2b09ee
Lucie Houdkova <lucachat@seznam.cz>
2023-11-13 16:43:30 +0100

    Oprava ukolu cislo 5 podle pokynu lektora.

--- src/Main.java
+++ src/Main.java
@@ -20,17 +20,28 @@ public static void ukol1() {
        System.out.println(cena);
     }
 
-   public static void ukol5() {
-       double cena = 0; // Nastavíme do proměnné hodnotu 0
+
+    public static void ukol5() {
+
+      BigDecimal cena = BigDecimal.valueOf(0); // Nastavíme do proměnné hodnotu 0
+
        // Desetkrát přičteme hodnotu jedna desetina (0,1):
+
        for (int i = 0; i < 10; i++) {
-            cena += 0.1;
-               // operátor "+=" znamená: přičti ke stávající
-               // hodnotě navíc 0.1
-        }
+
+           cena = cena.add(BigDecimal.valueOf(0.1));
+
+             // operátor "+=" znamená: přičti ke stávající
+
+             // hodnotě navíc 0.1
+
+       }
+
        System.out.println(cena);
-        // Má vypsat "1.0". Ale vypíše "0.9999" !!!
-    }
+
+       // Má vypsat "1.0". Ale vypíše "0.9999" !!!
+
+   }
 
     public static void ukol6() {
         BigDecimal cena = BigDecimal.valueOf(0);
